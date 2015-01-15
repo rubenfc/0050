2fd664fc8964c73e88e0979e84fbfd760c9991b9
unknown <rubenfc2013@gmail.com>
2015-01-15 15:43:30 +0100

    añadido el metodo raiz cuadrada

--- Test0050.java
+++ Test0050.java
@@ -7,75 +7,107 @@
  */
 public class Test0050
 {
-   public int multiplicacionEntera(int a, int b)
-   {
-       int incremento = a;
-       int tablaDeA = a;
-       int index = 0; 
-       while(index < (b-1))
-       {
-           index++;
-           tablaDeA = tablaDeA + incremento;    
-       }
-       
-       return tablaDeA;
-   }
-   
-  public int divisionEntera(int dividendo, int divisor)
-   {
+    public int multiplicacionEntera(int a, int b)
+    {
+        int incremento = a;
+        int tablaDeA = a;
+        int index = 0; 
+        while(index < (b-1))
+        {
+            index++;
+            tablaDeA = tablaDeA + incremento;    
+        }
 
-      int cociente = 0;
-      while(dividendo >= divisor )
-      {
-          dividendo = dividendo - divisor;
-          cociente++;
-      }
-       
-      return cociente;
-   }
-   
-  public int modulo(int dividendo, int divisor)
-  {
-   
-      while(dividendo >= divisor)
-      {
-          dividendo = dividendo - divisor;
-          
-      }
-      
-      return dividendo;
-  }
- 
-  public int potencia (int base, int esponente)
-  {
-      
-      int index = 0;
-      int resultado = base;
-      int resultadoTotal = resultado;
-      int contador = 1;
-      if(esponente == 0)
-      {
-          resultadoTotal = 1;
-      }
-      else if (esponente == 1)
-      {
-          resultadoTotal = base;
-      }
-      
-      else 
-      {   
-          while(index <= base)
-          {
-              resultado += base;
-              index ++;
-          }
-        
-          while(contador < esponente)
-          {
-              resultadoTotal += resultadoTotal;
-              contador ++;
-          }    
-      }
-      return resultadoTotal;
-  }
+        return tablaDeA;
+    }
+
+    public int divisionEntera(int dividendo, int divisor)
+    {
+
+        int cociente = 0;
+        while(dividendo >= divisor )
+        {
+            dividendo = dividendo - divisor;
+            cociente++;
+        }
+
+        return cociente;
+    }
+
+    public int modulo(int dividendo, int divisor)
+    {
+
+        while(dividendo >= divisor)
+        {
+            dividendo = dividendo - divisor;
+
+        }
+
+        return dividendo;
+    }
+
+    public int potencia (int base, int esponente)
+    {
+
+        int index = 0;
+        int resultado = base;
+        int resultadoTotal = resultado;
+        int contador = 1;
+        if(esponente == 0)
+        {
+            resultadoTotal = 1;
+        }
+        else if (esponente == 1)
+        {
+            resultadoTotal = base;
+        }
+
+        else 
+        {   
+            while(index <= base)
+            {
+                resultado += base;
+                index ++;
+            }
+
+            while(contador < esponente)
+            {
+                resultadoTotal += resultadoTotal;
+                contador ++;
+            }    
+        }
+        return resultadoTotal;
+    }
+
+    public int raiz(int numero)
+    {
+        int solucion = 2;
+        int contador = solucion;
+        int index = 0;
+        boolean resultado = false;
+        while(resultado == false && solucion <= 32)
+        {
+            contador += solucion;
+            index ++;
+            if(numero - contador == 0)
+            {
+                resultado = true;
+            }
+
+            else if(index > solucion)
+            {
+                index = 0;
+                solucion ++;
+                contador = solucion;
+            }
+        }
+
+        if(solucion == 32)
+        {
+            solucion = -1;
+            System.out.println("error");
+        }
+
+        return solucion;
+    }
 }
